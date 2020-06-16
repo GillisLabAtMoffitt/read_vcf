a2619cc82237645f867f2298d77901a37b7ed78a
chrisleitzinger <christelleleitzinger@gmail.com>
2020-06-16 16:48:34 -0400

    recode variables

--- 1.data loading.R
+++ 1.data loading.R
@@ -5,45 +5,45 @@ library(data.table)
 path <- fs::path("","Volumes","Gillis_Research","Christelle Colin-Leitzinger", "read VCF files")
 vcf <- read.delim(paste0(path, "/all_samples_sampleFiltered_bcfIsec.hg38_multianno_filtered_filtered_intersect.txt"),
                   na.strings = c("./.:.:.:.:.:.:.", "./.:.:.:.:.:.:.:.:.:."))
+vcf <- read.delim(file.choose(), na.strings = c("./.:.:.:.:.:.:.", "./.:.:.:.:.:.:.:.:.:."))
 
-# Unite all mutations characteristics as a row names
-vcf1 <- vcf %>%
+vcf <- vcf %>% 
+  # Unite all mutations characteristics as a row names
   unite(col = "X.CHROM_POS_ID_REF_ALT_QUAL_FILTER_INFO_FORMAT",
-        c("X.CHROM", "POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT"), sep = " ")
-
-# All us to pivot longer which create 1 row per mutation per patients, plus drop the mutations not found
-vcf2 <- vcf1 %>%
+        c("X.CHROM", "POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT"), sep = " ") %>% 
+  # pivot longer which create 1 row per mutation per patients
   pivot_longer(-X.CHROM_POS_ID_REF_ALT_QUAL_FILTER_INFO_FORMAT, 
                names_to = "patient_id", values_to = "DATA") %>% 
-  drop_na("DATA")
-
-vcf3 <- vcf2 %>% 
+  # drop the mutations not found
+  drop_na("DATA") %>% 
+  # re-separate the rownames to have mutation elements
   mutate(m_element = X.CHROM_POS_ID_REF_ALT_QUAL_FILTER_INFO_FORMAT) %>% 
   separate(m_element, 
            into = c("CHROM", "POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT"), 
            sep = " ")
+# Generate the gene element variables fron the INFO var
+vcf$GENE <- str_match(vcf$INFO, "Gene.ensGene=(.*?);GeneDetail.ensGene=")[,2]
+vcf$VARIANT_C <- str_match(vcf$INFO,"c.(.*?);esp6500siv2_all")
+vcf$VARIANT_P <- str_match(vcf$INFO, "p.(.*?);esp6500siv2_all")
+vcf$FUNCTION <- str_match(vcf$INFO, "ExonicFunc.knownGene=(.*?);")[,2] # Or can do "ExonicFunc.ensGene="
+vcf$COSMIC <- str_match(vcf$INFO, "OccurSum=(.*?);")[,2]
+vcf$ESP6500 <- str_match(vcf$INFO, "esp6500siv2_all=(.*?);")[,2]
 
-vcf3$GENE <- str_match(vcf3$INFO, "Gene.ensGene=(.*?);GeneDetail.ensGene=")[,2]
-vcf3$VARIANT_C <- str_match(vcf3$INFO,"c.(.*?);esp6500siv2_all")
-vcf3$VARIANT_P <- str_match(vcf3$INFO, "p.(.*?);esp6500siv2_all")
-vcf3$FUNCTION <- str_match(vcf3$INFO, "ExonicFunc.knownGene=(.*?);")[,2] # Or can do "ExonicFunc.ensGene="
-vcf3$COSMIC <- str_match(vcf3$INFO, "OccurSum=(.*?);")[,2]
-vcf3$ESP6500 <- str_match(vcf3$INFO, "esp6500siv2_all=(.*?);")[,2]
-
-vcf4 <- vcf3 %>% 
+vcf <- vcf %>% # separate FORMAT and DATA into format and data for corresponding value
   mutate(format = FORMAT) %>% 
   mutate(read = DATA) %>% 
-  separate(format, into = paste("format", 1:10, sep="_"), 
+  separate(format, into = paste("format", 1:10, sep="_"), # may not be necessary but can be useful
            sep = ":") %>% 
-  separate(read, into = paste("read", 1:10, sep="_"),
-           sep = ":") 
-
-
-vcf5 <- vcf4 %>% 
-    mutate(VAF = read_3) %>% 
-    mutate(DEPTH = read_4) %>% 
+  separate(read, into = paste("read", 1:10, sep="_"), # separate read aka DATA to be able to subset VAF and DEPTH
+           sep = ":") %>% 
+    mutate(VAF = read_3) %>% # genarate VAF and DEPTH var from read aka DATA
+    mutate(DEPTH = read_4) %>% # reorganize variable order
     select("patient_id", "CHROM", "POS", "REF", "ALT", "GENE", "VARIANT_C", 
            "VARIANT_P", "FUNCTION", "COSMIC", "ESP6500", "VAF", "DEPTH", "INFO", "FORMAT", "DATA",
            "format_1", "read_1", "format_2", "read_2", "format_3", "read_3", "format_4", "read_4", 
            "format_5", "read_5", "format_6", "read_6", "format_7", "read_7", "format_8", "read_8", 
            "format_9", "read_9", "format_10", "read_10") 
+
+
+write_csv(vcf, paste0(path, "/vcf.csv"))
+
