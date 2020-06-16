3ec10a862a1e616bc51898b460996773e8b003db
chrisleitzinger <christelleleitzinger@gmail.com>
2020-06-16 10:10:06 -0400

    Tidy data

--- 1.data loading.R
+++ 1.data loading.R
@@ -6,4 +6,19 @@ path <- fs::path("","Volumes","Gillis_Research","Christelle Colin-Leitzinger", "
 vcf <- read.delim(paste0(path, "/all_samples_sampleFiltered_bcfIsec.hg38_multianno_filtered_filtered_intersect.txt"),
                   na.strings = c("./.:.:.:.:.:.:.", "./.:.:.:.:.:.:.:.:.:."))
 
+# Unite all mutations characteristics as a row names
+colnames(vcf)
+vcf1 <- vcf %>%
+  unite(col = "X.CHROM_POS_ID_REF_ALT_QUAL_FILTER_INFO_FORMAT",
+        c("X.CHROM", "POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT"), sep = " * ")
+
+# All us to pivot longer which create 1 row per mutation per patients, plus drop the mutations not found
+vcf2 <- vcf1 %>%
+  pivot_longer(-X.CHROM_POS_ID_REF_ALT_QUAL_FILTER_INFO_FORMAT, 
+               names_to = "patient_id", values_to = "mutation") %>% 
+  drop_na("mutation")
+
+
+
+
 
